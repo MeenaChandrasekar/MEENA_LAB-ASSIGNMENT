40b96bdb4a8cc4b89913957d18b55963c3ce0084
Meena C <101164277+MeenaChandrasekar@users.noreply.github.com>
2022-03-25 10:10:33 +0530

    LAB ASSIGNMENT-4/Core Java

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+
+
+package day4;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a+b;
+	}
+
+	public Integer multiply(int a, int b) {
+		return a*b;
+	}
+
+	public Integer divide(int a, int b) {
+		return a/b;
+	}
+}
---
+++ FrequencytMain.java
@@ -0,0 +1,15 @@
+
+package Question1;
+
+import Harman_Question1.PrintFrequency;
+
+public class FrequencytMain
+{
+   static PrintFrequency f;
+
+	public static void main(String[] args) {
+	
+        System.out.println("The frequency of each word in the file");		
+		f=new PrintFrequency();
+}
+}
